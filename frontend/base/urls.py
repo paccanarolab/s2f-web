f5e86222d2cb1fa055c163ba08670908c313db25
Marcelo Baez <marcelobaezparaguay@gmail.com>
2025-12-01 18:39:52 -0300

    updating  frontend requirements and formatter job

--- frontend/base/urls.py
+++ frontend/base/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path("new-experiment", views.add_job, name="new-experiment"),
     path("api/ping", views.ping, name="api-ping"),
     path("api/pending_jobs", views.pending_jobs, name="api-pending-jobs"),
-    path("api/update_job_status",
-         views.job_update_status,
-         name="api-update-job-status"),
-    path("api/update_job_result",
-         views.job_update_result,
-         name="api-update-job-result"),
+    path(
+        "api/update_job_status", views.job_update_status, name="api-update-job-status"
+    ),
+    path(
+        "api/update_job_result", views.job_update_result, name="api-update-job-result"
+    ),
     path("api/job_details/<token>", views.job_details, name="api-job-details"),
     path("api/job_fasta/<token>", views.job_fasta, name="api-job-fasta"),
     path("api/job_annot/<token>", views.job_annot, name="api-job-annot"),
